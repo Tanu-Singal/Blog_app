314ceea066bde0418c0346f082d1a4889871c0af
Tanu-Singal <tanusingal7@gmail.com>
2025-01-31 05:07:03 +0530

    Update Bloglist.jsx

--- frontend/src/Component/Bloglist.jsx
+++ frontend/src/Component/Bloglist.jsx
@@ -7,7 +7,7 @@ const Bloglist = (props) => {
     const [blogs,setBlog]=useState([]);
     const fetchblog=async()=>{
     
-      const response=await fetch("http://localhost:3003/api/blog",{
+      const response=await fetch("https://blog-53.onrender.com/api/blog",{
         method:"GET"
       })
       if (!response.ok) {
