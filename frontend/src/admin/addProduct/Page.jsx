4f1dbe0180300da635c5ff9fde9dd6932d88a3fb
Tanu-Singal <tanusingal7@gmail.com>
2025-01-31 05:05:29 +0530

    Update Page.jsx

--- frontend/src/admin/addProduct/Page.jsx
+++ frontend/src/admin/addProduct/Page.jsx
@@ -30,7 +30,7 @@ const Page = () => {
     formData.append("authorImg", data.authorImg);
     formData.append("image", image); 
 
-    const response=await fetch("http://localhost:3003/upload",{
+    const response=await fetch("https://blog-53.onrender.com/upload",{
       method:"POST",
       body:formData,
     })
