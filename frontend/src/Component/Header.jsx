aae6cdb6c59bb0394c9472c030b9248ed2db9c33
Tanu-Singal <tanusingal7@gmail.com>
2025-01-31 05:07:38 +0530

    Update Header.jsx

--- frontend/src/Component/Header.jsx
+++ frontend/src/Component/Header.jsx
@@ -12,7 +12,7 @@ const Header = (props) => {
    e.preventDefault();
  
 
-   const response=await fetch("http://localhost:3003/api/email",{
+   const response=await fetch("https://blog-53.onrender.com/api/email",{
     method:"POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email }),
